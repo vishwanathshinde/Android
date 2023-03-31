6adecffc285ee3339f22f2440aa32d643c8748b6
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2023-03-31 18:04:06 +0530

    first commit

---
+++ Insert_Contact_Using_Intent/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.insert_contact_using_intent;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),New.class);
+                startActivity(i);
+            }
+
+        });
+    }
+}
---
+++ Insert_Contact_Using_Intent/New.java
@@ -0,0 +1,68 @@
+package com.example.insert_contact_using_intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class New extends Activity {
+    Button b1, b2;
+    ImageButton call, contact, call2, message2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        call = findViewById(R.id.call);
+        contact = findViewById(R.id.contact);
+        call2 = findViewById(R.id.call2);
+        message2 = findViewById(R.id.message2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(New.this, "Record Inserted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(New.this, "Record Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(New.this, "Nako Dabu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(New.this, "Nako Re Dabu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        call2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(New.this, "Nako Na", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        message2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(New.this, "Are Nako Re", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
